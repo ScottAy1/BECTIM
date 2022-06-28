1893bd38b044aace215da531a334444cfa5576d4
ScottAy1 <104260914+ScottAy1@users.noreply.github.com>
2022-06-28 18:26:41 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+const cpt = document.getElementsByClassName("btn1");
+
+function callback() {
+    document.getElementsByClassName("btn1").Innertext="Resident evil";
+}
+document.addEventListener("Click", callback);
